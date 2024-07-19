69baac4ba2f4abb3164333af7adaa0faf7d460d1
devanj628 <devipampireddy@gmail.com>
2024-07-19 16:36:33 +0530

    first committed

--- MyInfo.java
+++ MyInfo.java
@@ -2,24 +2,25 @@ import java.util.*;
 
 public class MyInfo{
     public static void main(String []args){
-        int age;
-        String FirstName;
-        String LastName;
-        String City;
-        Scanner sc= new Scannner(System.in);
-        FirstName=sc.nextLine();
+        Scanner sc= new Scanner(System.in);
+        String inputLine = scanner.nextLine();
+        String[] inputParts = inputLine.split(", ");
+        String FirstName = inputParts[0].trim();
+        String LastName = inputParts[1].trim();
+        int age = Integer.parseInt(inputParts[2].trim());
+        String City = inputParts[3].trim();
+    if (inputParts.length != 4) {
+        return;
+    }
         while(!FirstName.matches("[a-zA-Z]+")){
             FirstName=sc.nextLine();
         }
-        LastName=sc.nextLine();
         while(!LastName.matches("[a-zA-Z]+")){
             LastName=sc.nextLine();
         }
-        age=sc.nextInt();
         while(age<=1||age>=100){
             age=sc.nextInt();
         }
-        City=sc.nextLine();
         while(!City.matches("[a-zA-Z ]+")){
             City=sc.nextLine();
         }
